c8b8df25f5d2e557487d2b3f1083be26dbc51e43
xingkaizhu <18990361729@qq.com>
2017-10-24 13:09:40 +0800

    init2

---
+++ 111.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+int main (){
+		float a,b,c,d,e,f,x,y;
+	scanf("%f%f%f%f%f%f",&a,&b,&c,&d,&e,&f);
+	x=(f*b-c*e)/(b*d-e*f);
+	y=(f*a-c*d)/(e*a-b*d);
+	printf("x « %f£¨y « %f ",x,y);
+
+	return 0;
+}
+
+
+
+	
